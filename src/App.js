0cf4ab0808f1c5a0ea05082dd05150721a5cc2ac
Chris Zimmerman <chris@chriszimmerman.net>
2018-06-25 17:04:23 -0400

    Apply styling to App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ColorPicker from './ColorPicker';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -8,7 +9,9 @@ class App extends React.Component {
 
   render() {
       return (
-        <ColorPicker/>
+        <div class="app-container">
+          <ColorPicker/>
+        </div>
       );
   }
 }
