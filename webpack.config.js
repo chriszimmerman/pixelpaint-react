da4df487cbde28448c128620d2213c40406e5904
Chris Zimmerman <chris@chriszimmerman.net>
2018-06-25 17:04:16 -0400

    Allows for jest to work with js files that reference css files

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
           presets: ['es2015', 'react', 'stage-0']
         }
       },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
     ],
   },
   resolve: {
